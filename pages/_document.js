53ce073b162a3433848779d4762b641371508d29
Joel050505 <Joel.kilic@chasacademy.se>
2025-04-09 15:26:00 +0200

    `chore: Update image URL for Infinity running game`

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" href="/favicon.ico">
       <Head />
       <body className="antialiased">
         <Main />
